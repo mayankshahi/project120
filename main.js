7f1ba2924dafdb027a815fefc86d227d7fd2278e
mayankshahi <mayank.shahi@gmail.com>
2021-11-01 20:49:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+//function setup() {
+  //canvas = createCanvas(300, 300);
+  //canvas.position(600, 400);
+  //video = createCapture(VIDEO);
+  //video.hide();
+  //classifier = ml5.imageClassifier('Mobilenet', modelLoaded);
+//}
+
+//function modelLoaded() {
+  //console.log("Model Loaded!!");
+//}
+
+//function draw() {
+  //image(video, 0, 0, 300, 300);
+ // classifier.classify(video, gotResult);
+
+//}
+
+//function gotResult(error, results) {
+  //if (error) {
+ // console.error(error);
+  //}
+//
+  //else {
+    //console.log(results);
+    //document.getElementById("object_result").innerHTML = results[0].label;
+  //  document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
+  //}
+//}
+
+
+
+
+
+
+
